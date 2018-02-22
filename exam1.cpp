494af25dc4268fe70b628c9e292e997403812c9d
Unknown <noopang98@gmail.com>
2018-02-22 11:30:31 +0700

    ข้อ 1

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
 int main() {
-
-        return 0;
+    string line;
+    ifstream myile("example.tet");
+     if (myfile.is_open())
+     {
+       while (getline(myfile,line))
+       }
+          cout << line << '\n';
+       }
+       myfile.close();
+    }
+      else cout << "Unable to open file";
+      return 0;
 }
