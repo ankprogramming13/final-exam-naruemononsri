e81bd25909cae3dabf93dc4228e77ee8d4d07093
Unknown <noopang98@gmail.com>
2018-02-22 12:03:34 +0700

    ข้อ 1

--- exam1.cpp
+++ exam1.cpp
@@ -1,19 +1,65 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 using namespace std;
+int main()
+{
+    float number_1,number_2,result;
+    char choice_operator,choice_continue;
 
-int main() {
-    string line;
-    ifstream myile("example.tet");
-     if (myfile.is_open())
-     {
-       while (getline(myfile,line))
+    do
+    {
+
+
+        cout << "Enter Number : ";
+        cin >> number_1;
+        cout << "Enter Number : ";
+        cin >> number_2;
+
+        cout<<endl;
+        cout << "Select Operator" << endl;
+        cout << "A)ddition  (+)" << endl;
+        cout << "S)ubtraction  (-)" << endl;
+        cout << "M)ultiplication  (*)" << endl;
+        cout << "D)ivision (/)" << endl;
+        cout << "You Select : ";
+        cin >> choice_operator;
+
+        cout<<endl;
+        cout<<number_1;
+        switch(choice_operator)
+        {
+            case '+' :
+            case 'A' :
+            case 'a' : cout << " + ";
+                             result = number_1 + number_2;
+                             break;
+            case '-' :
+            case 'S' :
+            case 's' : cout << " - ";
+                             result = number_1 - number_2;
+                             break;
+            case '*' :
+            case 'M' :
+            case 'm' : cout << " * ";
+                              result = number_1 * number_2;
+                              break;
+            case '/' :
+            case 'D' :
+            case 'd' : cout << " / ";
+                             result = number_1 / number_2;
+                             break;
+        }
+          cout << number_2 << " = " << result << endl << endl;
+
+          cout << "Continue (Y/N) : ";
+          cin >> choice_continue;
        }
-          cout << line << '\n';
-       }
-       myfile.close();
-    }
-      else cout << "Unable to open file";
+        while(choice_continue == 'Y' || choice_continue == 'y');
+
+        cout << endl << "ExitProgram...";
+        cout << endl << endl;
+
+
+
+
       return 0;
 }
